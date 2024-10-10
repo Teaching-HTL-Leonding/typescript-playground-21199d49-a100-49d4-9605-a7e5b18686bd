e76e0ad2c188dc9668808ef2242bb784b4729a66
Rainer Stropek <rainer@timecockpit.com>
2024-10-10 13:52:32 +0200

    Saved in TypeScript Playground at 2024-10-10T11:52:32.245Z

---
+++ binary_guesser/index.ts
@@ -0,0 +1,14 @@
+let num = 0;
+
+function setup() {
+  num = Math.floor(random(0, 16));
+
+  createCanvas(400, 300);
+  background("black");
+
+  // <<< Add your code here
+}
+
+function mouseClicked() {
+  // <<< Add your code here
+}
