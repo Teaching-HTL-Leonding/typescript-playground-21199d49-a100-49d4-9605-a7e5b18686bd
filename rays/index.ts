d7eb4fbc686ad4661bde66b50320884ce58943ea
Rainer Stropek <rainer@timecockpit.com>
2024-11-13 19:49:10 +0100

    Saved in TypeScript Playground at 2024-11-13T18:49:10.514Z

---
+++ rays/index.ts
@@ -0,0 +1,15 @@
+const SIZE = 400;  // Canvas size (square)
+const MARGIN = 25; // Margin between the edges and the rays.
+                   // This is also the distance between the rays.
+
+function setup() {
+  createCanvas(SIZE, SIZE);
+  background("black");
+}
+
+function mouseMoved() {
+  background("black");
+  strokeWeight(2);
+
+  // <<< Add your code here
+}
